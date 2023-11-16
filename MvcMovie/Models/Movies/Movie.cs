9f1ecf6aa6753fca65956c478ae5e5d75af572cb
Bryan Jerahmeel Yao <bgyao@live.com>
2023-11-17 00:04:35 +0800

    Added UI-related fixes
    - Part 6 of the ASP.NET Core MVC tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/controller-methods-views?view=aspnetcore-8.0
    - Added Validation Messages on Models/Movies/Movie.cs (Self-study)

--- MvcMovie/Models/Movies/Movie.cs
+++ MvcMovie/Models/Movies/Movie.cs
@@ -1,15 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models.Movies;
 
 public class Movie
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter the movie title.")] //not covered by the tutorial. self-study
     public string? Title { get; set; }
 
+    [Display(Name ="Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
+    [Required(ErrorMessage = "Please enter the genre of the movie.")] //not covered by the tutorial. self-study
     public string? Genre { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
 }
