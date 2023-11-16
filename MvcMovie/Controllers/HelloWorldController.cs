e95122a19eb05e212aad9d4e8c438efb34711e3d
Bryan Jerahmeel Yao <bgyao@live.com>
2023-11-16 21:18:13 +0800

    Tutorial on Controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    //
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    //
+    // GET: /HelloWorld/Welcome
+    public string Welcome(string name, int Id = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello, {name}. ID: {Id}");
+    }
+}
